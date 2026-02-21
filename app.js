0065953cbdf46a02ef0ebf0f5cf77753916a7c7a
perrinbm97 <perrinbm08@gmail.com>
2026-02-21 17:20:50 -0500

    Added Functions

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 //Celsius to Fahrenheit Conversion
 
-let celsius = 10;
-let fahrenheit = (celsius*1.8) + 32;
+// let celsius = 10;
+// let fahrenheit = (celsius*1.8) + 32;
 
-console.log(fahrenheit);
+// console.log(fahrenheit);
+
+
+
+//Conversion using Functions
+
+//Named Function
+function tempconvert (degreeC){
+    return degreeC*1.8 + 32;
+}
+
+//Arrow Function
+// const tempconvert = (degreeC) => {
+//     return degreeC*1.8 + 32;
+// }
+
+console.log (tempconvert (30));
